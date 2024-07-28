fa933b50fb2fde2ed618db08575b45d3fc3e27a5
DESKTOP-VDMI65C\PC <contactwork.huytc@gmail.com>
2024-07-28 21:50:53 +0700

    Spring Security Basic Form

---
+++ src/test/java/com/security_example_01/HellospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security_example_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellospringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
